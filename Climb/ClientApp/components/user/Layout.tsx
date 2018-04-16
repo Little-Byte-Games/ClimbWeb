d12fe80ac835ece825184a1b8aed8c89184c4b43
Steve Yeager <steve.yeager@littlebytegames.com>
2018-04-15 21:39:51 -0700

    Add nav bar

--- Climb/ClientApp/components/user/Layout.tsx
+++ Climb/ClientApp/components/user/Layout.tsx
@@ -2,15 +2,25 @@
 import { Route } from "react-router-dom";
 import { Login } from "./Login";
 import { Register } from "./Register";
+import { Navbar } from "../_common/Navbar";
+import { ClimbClient } from "../../gen/climbClient";
+import ApplicationUser = ClimbClient.ApplicationUser;
 
-export class Layout extends React.Component {
+interface ILayoutProps {
+    user : ApplicationUser | null;
+}
+
+export class Layout extends React.Component<ILayoutProps> {
     public render() {
         return (
-            <div id="container">
-                <h1 id="title">Climb</h1>
-                <h2>User</h2>
-                <Route exact path="/user" component={ Login }/>
-                <Route exact path="/user/register" component={ Register }/>
+            <div>
+                <Navbar user={this.props.user} />
+                <div id="container">
+                    <h1 id="title">Climb</h1>
+                    <h2>User</h2>
+                    <Route exact path="/user" component={ Login }/>
+                    <Route exact path="/user/register" component={ Register }/>
+                </div>
             </div>
         );
     }
